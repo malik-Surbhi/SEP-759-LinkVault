aa1592f6567ba8a1d66379f344a884d0144f8b2a
Surbhi Malik <surbhimalik@Surbhis-MacBook-Pro.local>
2023-08-04 10:42:31 -0400

    Added new data in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ UserRole.destroy_all
 Role.destroy_all
 
 #Create an admin user
-admin_user = User.create!(
-  username: 'admin',
-  email: 'admin@example.com',
+admin = User.create!(
+  username: 'admin_user',
+  email: 'admin_user@example.com',
   password: '1234',
   password_confirmation: '1234',
   bio: 'Hi this is admin and I have the admin rights of this app'
@@ -21,28 +21,28 @@ admin_user = User.create!(
 
 # Attach profile photo to the admin user
 profile_photo_path = Rails.root.join('app', 'assets', 'images', 'admin_profile_photo.png')
-admin_user.profile_photo.attach(io: File.open(profile_photo_path), filename: 'admin_profile_photo.png')
+admin.profile_photo.attach(io: File.open(profile_photo_path), filename: 'admin_profile_photo.png')
 
 #Creating more users
 users = User.create! [
-                       {username: 'Surbhi', password: '1234', password_confirmation: '1234', email: 'surbhi@gmail.com', bio: 'Hey, this is Surbhi and this is my LinkTree'},
-                       {username: 'TestUser', password: '1234', password_confirmation: '1234', email: 'testuser@gmail.com', bio: 'Hey, this is TestUser and this is my LinkTree'},
-                       {username: 'Christine', password: '1234', password_confirmation: '1234', email: 'christine@gmail.com', bio: 'Hey, this is Christine and I enjoy reading books'},
+                       {username: 'Sumalik', password: '1234', password_confirmation: '1234', email: 'surbhimalik@gmail.com', bio: 'Hey, this is Surbhi and this is my LinkTree'},
+                       {username: 'Tester', password: '1234', password_confirmation: '1234', email: 'tester@gmail.com', bio: 'Hey, this is TestUser and this is my LinkTree'},
+                       {username: 'Inertia', password: '1234', password_confirmation: '1234', email: 'inertia@gmail.com', bio: 'Hey, this is Inertia and I enjoy reading books'},
                      ]
 
 #Adding profile photos to the two users
-surbhi = users[0];
-testUser = users[1];
-christine = users[2]
+sumalik = users[0];
+tester = users[1];
+inertia = users[2]
 
 surbhi_photo_path = Rails.root.join('app', 'assets', 'images', '1.png')
-surbhi.profile_photo.attach(io: File.open(surbhi_photo_path), filename: '1.png')
+sumalik.profile_photo.attach(io: File.open(surbhi_photo_path), filename: '1.png')
 
 testUser_photo_path = Rails.root.join('app', 'assets', 'images', '13.png')
-testUser.profile_photo.attach(io: File.open(testUser_photo_path), filename: '13.png')
+tester.profile_photo.attach(io: File.open(testUser_photo_path), filename: '13.png')
 
-christine_photo_path = Rails.root.join('app', 'assets', 'images', '3.png')
-christine.profile_photo.attach(io: File.open(christine_photo_path), filename: '3.png')
+inertia_photo_path = Rails.root.join('app', 'assets', 'images', '3.png')
+inertia.profile_photo.attach(io: File.open(inertia_photo_path), filename: '3.png')
 
 
 #Adding Links to users
@@ -50,11 +50,11 @@ links = Link.create! [
                        {title: 'Instagram', url: 'https://instagram.com/Surbhi', description: 'This is the link for instagram of Surbhi', user_id: users.first.id},
                        {title: 'Facebook', url: 'https://facebook.com/Surbhi', description: 'This is the link for Facebook of Surbhi', user_id: users.first.id},
                        {title: 'Youtube', url: 'https://youtube.com/testUser', description: 'This is the link for Youtube of TestUser',  user_id: users.second.id},
-                       {title: 'Ruby On Rails', url: 'https://rubyOnRails.com/admin', description: 'This is the link for Ruby on Rails of Admin',  user_id: admin_user.id},
-                       {title: 'Youtube Vlog', url: 'https://youtube.com/admin', description: 'This is the link for my Youtube channel ',  user_id: admin_user.id},
-                       {title: 'Medium Post', url: 'https://medium.com/new_post', description: 'This is the link for my Medium post on the latest dev work ',  user_id: admin_user.id},
-                       {title: 'Reddit discussion', url: 'https://reddit.com/findings', description: 'Linking the latest reddit post in which we had discussion ',  user_id: christine.id},
-                       {title: 'My Website', url: 'https://godaddy.com/christine', description: 'Welcome to my Website that I created recently! ',  user_id: christine.id}
+                       {title: 'Ruby On Rails', url: 'https://rubyOnRails.com/admin', description: 'This is the link for Ruby on Rails of Admin',  user_id: admin.id},
+                       {title: 'Youtube Vlog', url: 'https://youtube.com/admin', description: 'This is the link for my Youtube channel ',  user_id: admin.id},
+                       {title: 'Medium Post', url: 'https://medium.com/new_post', description: 'This is the link for my Medium post on the latest dev work ',  user_id: admin.id},
+                       {title: 'Reddit discussion', url: 'https://reddit.com/findings', description: 'Linking the latest reddit post in which we had discussion ',  user_id: inertia.id},
+                       {title: 'My Website', url: 'https://godaddy.com/christine', description: 'Welcome to my Website that I created recently! ',  user_id: inertia.id}
                      ]
 
 #Assigning roles to users
@@ -65,13 +65,13 @@ roles = Role.create! [
 
 #Assigning the admin role
 role = Role.find_by name: 'admin'
-UserRole.create! [{user_id: admin_user.id, role_id: role.id}]
-UserRole.create! [{user_id: christine.id, role_id: role.id}]
+UserRole.create! [{user_id: admin.id, role_id: role.id}]
+UserRole.create! [{user_id: inertia.id, role_id: role.id}]
 
 #Assigning User role
 roleUser = Role.find_by name: 'user'
-UserRole.create! [{user_id: surbhi.id, role_id: roleUser.id}]
-UserRole.create! [{user_id: testUser.id, role_id: roleUser.id}]
+UserRole.create! [{user_id: sumalik.id, role_id: roleUser.id}]
+UserRole.create! [{user_id: tester.id, role_id: roleUser.id}]
 
 
 
